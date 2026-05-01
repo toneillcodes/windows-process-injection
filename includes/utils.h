f5e38b1b39bb63a9875205eb9065a922ac3c07e6
toneillcodes <148013535+toneillcodes@users.noreply.github.com>
2026-05-01 08:57:16 -0400

    Add GetExplorerPID to utils.h

--- includes/utils.h
+++ includes/utils.h
@@ -10,5 +10,6 @@ int my_strlen_for(const char* s);
 int my_strcmp(const char* s1, const char* s2);
 int my_stricmp(const char* s1, const char* s2);
 int my_wcsicmp(const wchar_t* s1, const wchar_t* s2);
+DWORD GetExplorerPID();
 
 #endif 
